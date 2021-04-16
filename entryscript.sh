d8553f246fcf804ecd31d99140a38a3b848bf401
Sowjanya Padala <sowjanyapadala15@gmail.com>
2021-04-15 22:34:30 -0400

    lll

--- entryscript.sh
+++ entryscript.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-sudo yum update -y && sudo yum install docker -y
-sudo systemctl start docker
+sudo yum update -y && sudo yum install -y docker
+sudo systemctl start docker 
 sudo usermod -aG docker ec2-user
 docker run -p 8080:80 nginx
-            
+
